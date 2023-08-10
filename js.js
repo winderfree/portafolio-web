b0f4db3974f591dbed099998c502cf4dff8b10bd
Juan Casado Luna <juans.casado@gmail.com>
2023-08-10 18:24:59 -0400

    Add Fotos De Lenguajes

---
+++ js.js
@@ -0,0 +1,12 @@
+console.log("Hola!");
+
+const foto_html = document.querySelector("#html");
+const foto_css3 = document.querySelector("#css5");
+const foto_javascript = document.querySelector("#javascript");
+const foto_python = document.querySelector("#python");
+
+foto_html.src = "imagenes/fotos-tech-inow/html.png";
+foto_css3.src = "imagenes/fotos-tech-inow/css3.png";
+foto_javascript.src = "imagenes/fotos-tech-inow/javascript.png";
+foto_python.src = "imagenes/fotos-tech-inow/python.png";
+
