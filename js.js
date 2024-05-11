30b0f30c575cad1a99d4a24236dbaf44c7f438e2
Juan Casado Luna <juans.casado@gmail.com>
2024-05-11 12:04:22 -0400

    Update Index

--- js.js
+++ js.js
@@ -3,15 +3,16 @@ console.log("Hola!");
 const foto_html = document.querySelector("#html5");
 const foto_css3 = document.querySelector("#css");
 const foto_javascript = document.querySelector("#javascript");
+const foto_c = document.querySelector("#c_sharp");
 const foto_python = document.querySelector("#python");
 const foto_django = document.querySelector("#django");
 const foto_react = document.querySelector("#react");
 
 foto_html.src = "imagenes/fotos-tech-inow/html5.png";
 foto_css3.src = "imagenes/fotos-tech-inow/css.png";
-foto_javascript.src = "imagenes/fotos-tech-inow/javaScript.png";
+foto_javascript.src = "imagenes/fotos-tech-inow/javascript.png";
 foto_python.src = "imagenes/fotos-tech-inow/python.png";
 foto_django.src = "imagenes/fotos-tech-inow/django.png";
 foto_react.src = "imagenes/fotos-tech-inow/react.png";
-
+foto_c.src = "imagenes/fotos-tech-inow/c_sharp.png";
 
