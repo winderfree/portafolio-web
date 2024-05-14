f88f6d6fb183bb620c770aefeb03d0de2932404a
Juan Casado Luna <juans.casado@gmail.com>
2024-05-14 12:22:24 -0400

    Update Index FotosUper 2nd

--- js.js
+++ js.js
@@ -2,17 +2,23 @@ console.log("Hola!");
 
 const foto_html = document.querySelector("#html5");
 const foto_css3 = document.querySelector("#css");
-const foto_javascript = document.querySelector("#JavaScript");
-const foto_c = document.querySelector("#c_sharp");
-const foto_python = document.querySelector("#Python");
+const foto_javascript = document.querySelector("#javascript");
+const foto_php = document.querySelector("#php");
+const foto_c_sharp = document.querySelector("#c_sharp");
+const foto_python = document.querySelector("#python");
 const foto_django = document.querySelector("#django");
 const foto_react = document.querySelector("#react");
+const foto_codeigniter = document.querySelector("#codeigniter");
+const foto_laravel = document.querySelector("#laravel");
 
 foto_html.src = "imagenes/fotos-tech-inow/html5.png";
 foto_css3.src = "imagenes/fotos-tech-inow/css.png";
 foto_javascript.src = "imagenes/fotos-tech-inow/javascript.png";
+foto_php.src = "imagenes/fotos-tech-inow/php.png";
+foto_c_sharp.src = "imagenes/fotos-tech-inow/c_sharp.png";
 foto_python.src = "imagenes/fotos-tech-inow/python.png";
 foto_django.src = "imagenes/fotos-tech-inow/django.png";
 foto_react.src = "imagenes/fotos-tech-inow/react.png";
-foto_c.src = "imagenes/fotos-tech-inow/c_sharp.png";
+foto_codeigniter.src = "imagenes/fotos-tech-inow/codeigniter.png";
+foto_laravel.src = "imagenes/fotos-tech-inow/laravel.png";
 
