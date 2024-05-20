41c6c25cf524bd453139f194d3f39cea1e25ad8e
Juan Casado Luna <juans.casado@gmail.com>
2024-05-20 19:09:31 -0400

    portafolio-version-1.0

--- js.js
+++ js.js
@@ -10,6 +10,9 @@ const foto_django = document.querySelector("#django");
 const foto_react = document.querySelector("#react");
 const foto_codeigniter = document.querySelector("#codeigniter");
 const foto_laravel = document.querySelector("#laravel");
+const foto_ts = document.getElementById("ts");
+const foto_angular = document.getElementById("angular");
+
 
 foto_html.src = "imagenes/fotos-tech-inow/html5.png";
 foto_css3.src = "imagenes/fotos-tech-inow/css.png";
@@ -17,8 +20,10 @@ foto_javascript.src = "imagenes/fotos-tech-inow/javascript.png";
 foto_php.src = "imagenes/fotos-tech-inow/php.png";
 foto_c_sharp.src = "imagenes/fotos-tech-inow/c_sharp.png";
 foto_python.src = "imagenes/fotos-tech-inow/python.png";
+foto_ts.src = "imagenes/fotos-tech-inow/ts.png";
 foto_django.src = "imagenes/fotos-tech-inow/django.png";
 foto_react.src = "imagenes/fotos-tech-inow/react.png";
 foto_codeigniter.src = "imagenes/fotos-tech-inow/codeigniter.png";
 foto_laravel.src = "imagenes/fotos-tech-inow/laravel.png";
+foto_angular.src = "imagenes/fotos-tech-inow/angular.png";
 
